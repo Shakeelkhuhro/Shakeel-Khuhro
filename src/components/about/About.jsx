0379b033947a23d6ca06d17f1f9a92beb92c214d
Shakeel Khuhro <shakeelahmedkhuhro5454@gmail.com>
2024-09-26 19:45:02 +0500

    Updated About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./about.css";
-import AboutImage from "../../assets/me-2.jpg";
+import AboutImage from "../../assets/me-2.jpeg";
 import TabTitles from "./TabTitles";
 
 const About = () => {
@@ -15,8 +15,8 @@ const About = () => {
       <h5>Get to Know</h5>
       <h2 className="sub-title">About Me</h2>
       <p className="about-text">
-        Hello, I'm Shakeel Khuhro, a Cloud Engineer specializing in Azure and Google Cloud Platform (GCP), with additional expertise as a MERN Stack Developer specializing in Firebase. As a GOLD Student Ambassador and EMEA Regional Lead at Microsoft Learn, I am deeply involved in fostering learning and innovation within the tech community.
-                  I invite you to explore my portfolio, where you'll find examples of my Cloud and MERN projects. From designing scalable cloud architectures to crafting feature-rich web applications, my work reflects a dedication to excellence and innovation. I am open to connecting for potential opportunities and collaboration. Thank you for considering my profile.
+        I am Shakeel Khuhro, a React Developer currently working at Hifah Technologies. I am also a Microsoft Certified Azure Administrator and have taken on the role of a Gold Microsoft Learn Student Ambassador and the EMEA Regional Lead. Previously, I served as the Marketing Manager at MLSA MUET Khairpur Mir's until December 2023. 
+        Additionally, I have spoken on different technologies, empowering over 5,000 individuals both online and offline. Through my journey, I actively contribute to the tech community, sharing knowledge and collaborating with others.
       </p>
       <div className="container about-container">
         <div className="about-me">
@@ -35,6 +35,42 @@ const About = () => {
             {activeTab === "skills" && (
               <ul>
                 <li>
+                  <span className="skills-span">JavaScript</span>
+                  <div className="skill-line">
+                    <div
+                      className="skill-progress"
+                      style={{ width: "80%" }}
+                    ></div>
+                  </div>
+                </li>
+                <li>
+                  <span className="skills-span">React native</span>
+                  <div className="skill-line">
+                    <div
+                      className="skill-progress"
+                      style={{ width: "80%" }}
+                    ></div>
+                  </div>
+                </li>
+                <li>
+                  <span className="skills-span">ReactJS</span>
+                  <div className="skill-line">
+                    <div
+                      className="skill-progress"
+                      style={{ width: "90%" }}
+                    ></div>
+                  </div>
+                </li>
+                <li>
+                  <span className="skills-span">NextJS</span>
+                  <div className="skill-line">
+                    <div
+                      className="skill-progress"
+                      style={{ width: "70%" }}
+                    ></div>
+                  </div>
+                </li>
+                <li>
                   <span className="skills-span">Microsoft Azure</span>
                   <div className="skill-line">
                     <div
@@ -44,7 +80,7 @@ const About = () => {
                   </div>
                 </li>
                 <li>
-                  <span className="skills-span">Google Cloud Platform</span>
+                  <span className="skills-span">AWS Cloud</span>
                   <div className="skill-line">
                     <div
                       className="skill-progress"
@@ -62,65 +98,11 @@ const About = () => {
                   </div>
                 </li>
                 <li>
-                  <span className="skills-span">JavaScript</span>
-                  <div className="skill-line">
-                    <div
-                      className="skill-progress"
-                      style={{ width: "85%" }}
-                    ></div>
-                  </div>
-                </li>
-                <li>
-                  <span className="skills-span">ReactJS</span>
-                  <div className="skill-line">
-                    <div
-                      className="skill-progress"
-                      style={{ width: "80%" }}
-                    ></div>
-                  </div>
-                </li>
-                <li>
-                  <span className="skills-span">TypeScript</span>
-                  <div className="skill-line">
-                    <div
-                      className="skill-progress"
-                      style={{ width: "70%" }}
-                    ></div>
-                  </div>
-                </li>
-                <li>
-                  <span className="skills-span">NextJS</span>
-                  <div className="skill-line">
-                    <div
-                      className="skill-progress"
-                      style={{ width: "70%" }}
-                    ></div>
-                  </div>
-                </li>
-                <li>
-                  <span  className="skills-span">NodeJS</span>
-                  <div className="skill-line">
-                    <div
-                      className="skill-progress"
-                      style={{ width: "65%" }}
-                    ></div>
-                  </div>
-                </li>
-                <li>
                   <span className="skills-span">Git</span>
                   <div className="skill-line">
                     <div
                       className="skill-progress"
                       style={{ width: "75%" }}
-                    ></div>
-                  </div>
-                </li>
-                <li>
-                  <span className="skills-span">Firebase</span>
-                  <div className="skill-line">
-                    <div
-                      className="skill-progress"
-                      style={{ width: "60%" }}
                     ></div>
                   </div>
                 </li>
@@ -136,16 +118,16 @@ const About = () => {
           >
             <ul>
               <li>
-                <span>Dec-2022 - Current</span>
-                <br /> Front-End Developer at Paidlance
+                <span>June 2024 - Current</span>
+                <br /> React Developer at Hifah Technologies
               </li>
               <li>
-                <span>Jan-2023 - Current </span>
-                <br /> Beta Student Ambassador at Microsoft Learn
+                <span>Jan 2023 - Current </span>
+                <br /> Gold Student Ambassador & EMEA Regional Lead at Microsoft Learn
               </li>
               <li>
-                <span>Dec-2022 - Current</span>
-                <br /> Marketing Manager at MLSA MUET Khp
+                <span>Dec 2022 - Dec 2023</span>
+                <br /> Marketing Manager at MLSA MUET Khairpur
               </li>
             </ul>
           </div>
@@ -157,16 +139,22 @@ const About = () => {
           >
             <ul>
               <li>
-                <span>Jun-2022 - July-2022</span>
-                <br /> Front-End Development (HTML,CSS,JS)
-                <br />
-                <small>Saylani Mass Information Technology</small>{" "}
-              </li>
-              <li>
-                <span>April-2023 - Continue</span>
-                <br /> Front-End Development (React and NextJS)
+                <span>April-2023 - June-2023</span>
+                <br /> Front-End Development
                 <br />
                 <small>Bytewise Limited</small>{" "}
+              </li>
+              <li>
+                <span>May-2024 - July-2024</span>
+                <br /> Azure Cloud
+                <br />
+                <small>Bytewise Limited</small>{" "}
+              </li>
+              <li>
+                <span>May-2024 - June-2024</span>
+                <br /> CCNA and Azure Enterprise
+                <br />
+                <small>Corvit System</small>{" "}
               </li>
               <li>
                 <span>2021 - Continue</span>
